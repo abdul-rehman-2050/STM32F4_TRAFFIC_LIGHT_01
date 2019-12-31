a72ff4fa50df97d5774944b407e684364940795a
abdul-rehman-2050 <supermind2002@gmail.com>
2019-12-31 05:20:06 +0500

    added proximity sensor and a new state

--- src/main.c
+++ src/main.c
@@ -9,13 +9,20 @@
 
 #include "stm32f4xx.h"
 
-GPIO_InitTypeDef gpio;
+
 
 #define LED_PORT	GPIOE
 #define LED_Red 	GPIO_Pin_7
 #define LED_Yellow	GPIO_Pin_9
 #define LED_Green 	GPIO_Pin_11
 #define LED_CLK 	RCC_AHB1Periph_GPIOE
+
+// IR Proximity Sensor Pin declaration
+#define IR_PROXY_PORT 	GPIOE
+#define IR_PROXY_CLK	RCC_AHB1Periph_GPIOE
+#define IR_PROXY_PIN	GPIO_Pin_5
+
+
 
 void delay_ms(uint32_t ms){
 	ms *= 20000;
@@ -24,11 +31,25 @@ void delay_ms(uint32_t ms){
 	}
 }
 
-void setup(){
+void LEDs_init(){
+	GPIO_InitTypeDef gpio;
 	RCC_AHB1PeriphClockCmd(LED_CLK,ENABLE);//ENABLE CLOCK FIRST
 	gpio.GPIO_Mode = GPIO_Mode_OUT;
 	gpio.GPIO_Pin = LED_Red | LED_Yellow | LED_Green;
 	GPIO_Init(LED_PORT,&gpio);
+}
+
+void IR_PROXY_init(){
+	GPIO_InitTypeDef gpio;
+	RCC_AHB1PeriphClockCmd(IR_PROXY_CLK,ENABLE);//ENABLE CLOCK FIRST
+	gpio.GPIO_Mode = GPIO_Mode_IN;
+	gpio.GPIO_Pin = IR_PROXY_PIN;
+	GPIO_Init(IR_PROXY_PORT,&gpio);
+
+}
+void setup(){
+	LEDs_init();
+	IR_PROXY_init();
 }
 
 int main(void)
@@ -63,6 +84,14 @@ int main(void)
 	  GPIO_SetBits(LED_PORT,LED_Yellow);
 	  GPIO_ResetBits(LED_PORT,LED_Green);
 	  delay_ms(1000);
+
+	  while(!GPIO_ReadInputDataBit(IR_PROXY_PORT,IR_PROXY_PIN)){
+		  //Fourth Pattern [Signal is Yellow ]
+		 GPIO_ResetBits(LED_PORT,LED_Red);
+		 GPIO_ToggleBits(LED_PORT,LED_Yellow);
+		 GPIO_ResetBits(LED_PORT,LED_Green);
+		 delay_ms(1000);
+	  }
 
 
 
